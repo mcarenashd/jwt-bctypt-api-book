0e20e5aed48d56492739e033a07b482a52c3306f
Camila Arenas <mcarenasdh@gmail.com>
2025-10-06 11:24:56 +0200

    Fix: fix update route issue

--- routes/bookRoutes.js
+++ routes/bookRoutes.js
@@ -1,11 +1,11 @@
 import express from 'express'
-import {getAllBooks, createBook, deleteBook} from '../controllers/BookController.js'
+import {getAllBooks, createBook, deleteBook, updateBook} from '../controllers/BookController.js'
 import { authMiddleware } from '../middleware/authMiddleware.js'
 
 const bookRouter = express.Router()
 bookRouter.get("/", getAllBooks)
 bookRouter.post("/", authMiddleware, createBook)
 bookRouter.delete("/:id", deleteBook)
-// bookRouter.update("/:id", updateBook)
+bookRouter.put("/:id", updateBook)
 
 export default bookRouter
